6da04babdd9bbb4c8edd604d449d2aee7867f59b
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-06 14:37:22 +0200

    undefined print

--- script.js
+++ script.js
@@ -15,11 +15,67 @@ Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i 
 #Milestone 3
 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. */
 
-
+console.log("js ok")
+//array di oggetti
 const data = [{postId: 1,
-                nomeAutore: "Phil Mangione",
-                fotoSrcAutore: "https://unsplash.it/300/300?image=3",
-                data: "07/06/2022",
-                testoPost: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto.",
-                fotoSrcPost: "https://unsplash.it/300/300?image=1",
-                likes: 28}]
+            nomeAutore: "Phil Mangione",
+            fotoSrcAutore: "https://picsum.photos/200",
+            dataPost: "07/06/2022",
+            testoPost: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto.",
+            fotoSrcPost: "https://picsum.photos/200",
+            likes: 28}]
+
+const printPlace = document.getElementById("container")
+
+
+//creo una funzione stampa carte 
+
+function getPost(){
+
+
+    printPlace.innerHTML = `<div class="post">
+    <div class="post__header">
+      <div class="post-meta">
+        <div class="post-meta__icon">
+          <img class="profile-pic" src=${data.fotoSrcAutore} alt="${data.nomeAutore}" />
+        </div>
+        <div class="post-meta__data">
+          <div class="post-meta__author">${data.postId}</div>
+          <div class="post-meta__time">${data.dataPost}</div>
+        </div>
+      </div>
+    </div>
+    <div class="post__text">
+    ${data.testoPost}
+    </div>
+    <div class="post__image">
+      <img src=${data.fotoSrcPost} alt="" />
+    </div>
+    <div class="post__footer">
+      <div class="likes js-likes">
+        <div class="likes__cta">
+          <a class="like-button js-like-button" href="#" data-postid="${data.postId}">
+            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+            <span class="like-button__label">Mi Piace</span>
+          </a>
+        </div>
+        <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${data.likes}</b> persone</div>
+      </div>
+    </div>
+  </div>`
+
+  return 
+
+}
+
+getPost()
+
+
+
+console.log(data)
+
+
+
+   
+
+    
